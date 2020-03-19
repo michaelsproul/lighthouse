4bbe2af0ed5e1cd2cf540b711412cc536ae37d80
Adam Szkoda <adaszko@gmail.com>
2020-03-19 17:46:45 +0100

    Fix broken test case

--- eth2/types/src/beacon_state.rs
+++ eth2/types/src/beacon_state.rs
@@ -93,10 +93,8 @@ impl AllowNextEpoch {
     }
 }
 
-
 #[derive(Debug, PartialEq, Eq, Hash)]
 pub struct BeaconStateHash(Hash256);
-
 
 impl From<Hash256> for BeaconStateHash {
     fn from(hash: Hash256) -> BeaconStateHash {
@@ -104,13 +102,11 @@ impl From<Hash256> for BeaconStateHash {
     }
 }
 
-
 impl From<BeaconStateHash> for Hash256 {
     fn from(beacon_state_hash: BeaconStateHash) -> Hash256 {
         beacon_state_hash.0
     }
 }
-
 
 /// The state of the `BeaconChain` at some slot.
 ///
